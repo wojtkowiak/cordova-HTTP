2a528de25e5697e710e9860ce511726b2ad8c1c7
Tommy-Carlos Williams <tommy@devgeeks.org>
2015-08-06 09:51:26 +1000

    Increased iOS timeout from 60 seconds to 5 minutes
    Also set a new timeout for Android of 5 minutes... it's probable that it
    used to wait forever.

--- src/ios/AFNetworking/AFHTTPRequestOperationManager.m
+++ src/ios/AFNetworking/AFHTTPRequestOperationManager.m
@@ -117,6 +117,8 @@
                         failure:(void (^)(AFHTTPRequestOperation *operation, NSError *error))failure
 {
     NSMutableURLRequest *request = [self.requestSerializer requestWithMethod:@"GET" URLString:[[NSURL URLWithString:URLString relativeToURL:self.baseURL] absoluteString] parameters:parameters error:nil];
+    // Change request timeout from default 60 seconds to 5 minutes
+    [request setTimeoutInterval:300];
     AFHTTPRequestOperation *operation = [self HTTPRequestOperationWithRequest:request success:success failure:failure];
 
     [self.operationQueue addOperation:operation];
